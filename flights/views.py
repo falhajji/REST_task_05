6fde659009f55ecd6841273f4b44e6865016e9b1
Fatemah Al-Hajji Boukhadour <f.alhajji@gmail.com>
2019-07-25 18:24:56 +0300

    this was tricky but kind of cool, shoutout to Mariam for her help" git push
    "

--- flights/views.py
+++ flights/views.py
@@ -1,8 +1,8 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView, DestroyAPIView, CreateAPIView
 from datetime import datetime
-
+from rest_framework.permissions import BasePermission
 from .models import Flight, Booking
-from .serializers import FlightSerializer, BookingSerializer, BookingDetailsSerializer, UpdateBookingSerializer, RegisterSerializer
+from .serializers import FlightSerializer, BookingSerializer, BookingDetailsSerializer, UpdateBookingSerializer, RegisterSerializer, UpdateBookingSerializerUser
 
 
 class FlightsList(ListAPIView):
@@ -11,8 +11,10 @@ class FlightsList(ListAPIView):
 
 
 class BookingsList(ListAPIView):
-	queryset = Booking.objects.filter(date__gte=datetime.today())
 	serializer_class = BookingSerializer
+
+	def get_queryset(self):
+		return Booking.objects.filter(date__gte=datetime.today(), user = self.request.user)
 
 
 class BookingDetails(RetrieveAPIView):
@@ -24,9 +26,15 @@ class BookingDetails(RetrieveAPIView):
 
 class UpdateBooking(RetrieveUpdateAPIView):
 	queryset = Booking.objects.all()
-	serializer_class = UpdateBookingSerializer
 	lookup_field = 'id'
 	lookup_url_kwarg = 'booking_id'
+
+	def get_serializer_class(self):
+		if self.request.user.is_staff:
+			return UpdateBookingSerializer
+		else:
+			return UpdateBookingSerializerUser
+
 
 
 class CancelBooking(DestroyAPIView):
